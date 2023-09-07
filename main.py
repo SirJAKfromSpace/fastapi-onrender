0dc19645dcdc739fb35318151f50ca619ce8c2eb
Jawad Aziz Khan <jawadazizkhan.jak@gmail.com>
2023-09-06 17:41:35 -0700

    ping

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from tinydb import TinyDB, Query
+from fastapi import FastAPI, Request
+import time
+# from typing import Optional
 
 app = FastAPI()
+db = TinyDB('tinydb.json')
+pingdb = db.table('ping')
+userdb = db.table('user')
+datadb = db.table('data')
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.get("/ping")
+async def root(request: Request):
+    print('PING')
+    useragent = str(request._headers.get('user-agent'))
+    timestamp = int(time.time())
+    pingdata = {'agent': useragent, 'timestamp': timestamp}
+    pid = pingdb.insert(pingdata)
+    return {'idnumber': pid, 'ping': pingdata}
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
